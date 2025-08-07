4981a94b162567cceb1c8e9df41bc42673e1aca4
Marvin-desmond <marvindesmond@gmail.com>
2025-08-07 00:08:14 -0400

    build the engine for initializing the model and tokenizer, and generate tokens on stream

--- src/tiny_sota/inference/engines.py
+++ src/tiny_sota/inference/engines.py
@@ -1,19 +1,38 @@
-from enum import Enum
-from .utils import Llama3Tokenizer, Qwen3Tokenizer
+import torch 
+from dataclasses import dataclass
+from .tokenizers import (
+    Llama3Tokenizer, Qwen3Tokenizer
+)
+from ..tiny_utils import ColorPrint
+from .utils import generate_text_stream, GenerateConfig 
 
-class Tokenizers(Enum):
+@dataclass
+class TokenizerChoices:
     qwen = Qwen3Tokenizer 
     llama3 = Llama3Tokenizer
 
-def loadTokenizer(file, tokenizer: Tokenizers):
-    tokenizer = tokenizer(file)
-    return tokenizer
+def loadTokenizer(file, tokenizer_class: TokenizerChoices, **kwargs):
+    tokenizer_object = tokenizer_class(file, **kwargs)
+    return tokenizer_object
 
 class LLMEngine():
-    def __init__(self, model_file, tokenizer_file: str, tokenizer_opt: Tokenizers):
-        self.model_file = model_file 
-        self.tokenizer = loadTokenizer(tokenizer_file) 
-    def __call__(self, prompt):
+    def __init__(self, 
+                 loaded_model, 
+                 tokenizer_file: str, tokenizer_choice:TokenizerChoices, 
+                 **kwargs):
+        self.model = loaded_model.eval() 
+        self.tokenizer = loadTokenizer(tokenizer_file, tokenizer_choice, **kwargs) 
+        self.eos_token_id = self.tokenizer.eos_token_id
+    def __call__(self, prompt, g_config: GenerateConfig):
+        max_new_tokens = g_config.max_new_tokens
+        context_len = g_config.context_len
+        device = g_config.device
         tokens = self.tokenizer.encode(prompt)
-        words = self.tokenizer.decode(tokens)
-    
+        token_ids = torch.tensor(tokens,device=device).unsqueeze(0)
+        for token in generate_text_stream(
+            self.model, token_ids, max_new_tokens, 
+            context_len, eos_token_id=self.eos_token_id):
+            token_id = token.squeeze(0).tolist()
+            print("hello")
+            # print(self.tokenizer.decode(token_id),
+            #         end="", flush=True)
