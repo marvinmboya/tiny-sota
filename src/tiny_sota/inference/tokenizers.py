3d9129f447edc123893ea387a1f1b12b04ed0f62
Marvin-desmond <marvindesmond@gmail.com>
2025-08-06 20:00:24 -0400

    implement class tokenizers for qwen, llama

---
+++ src/tiny_sota/inference/tokenizers.py
@@ -0,0 +1,77 @@
+import re
+from pathlib import Path 
+
+from tokenizers import Tokenizer
+import tiktoken
+from tiktoken.load import load_tiktoken_bpe
+
+class Llama3Tokenizer:
+    SPLIT_RE = re.compile(r"(<\|[^>]+?\|>)")
+    def __init__(self, file: Path):
+        self.special_to_id = {"<|begin_of_text|>": 128000,
+        "<|end_of_text|>": 128001, "<|start_header_id|>": 128006,
+        "<|end_header_id|>": 128007, "<|eot_id|>": 128009}
+        self.tokenizer = Tokenizer.from_file(str(file))
+    def encode(self, text, bos=False, eos=False):
+        ids = []
+        stripped = text.strip()
+        if stripped in self.special_to_id and "\n" not in stripped:
+            return [self.special_to_id[stripped]]
+        print(f">>> {list(filter(None, self.SPLIT_RE.split(text)))}")
+        for part in filter(None, self.SPLIT_RE.split(text)):
+            if part in self.special_to_id:
+                ids.append(self.special_to_id[part])
+            else:
+                ids.extend(self.tokenizer.encode(part).ids)
+        return ids
+    def decode(self, ids):
+        return self.tokenizer.decode(ids, skip_special_tokens=False)
+
+
+class Qwen3Tokenizer:
+    _SPECIALS = [
+        "<|endoftext|>", "<|im_start|>", "<|im_end|>",
+        "<|object_ref_start|>", "<|object_ref_end|>",
+        "<|box_start|>", "<|box_end|>","<|quad_start|>", 
+        "<|quad_end|>","<|vision_start|>", "<|vision_end|>",
+        "<|vision_pad|>", "<|image_pad|>", "<|video_pad|>",
+    ]
+    SPLIT_RE = re.compile(r"(<\|[^>]+?\|>)")
+    def __init__(self, file: Path, think_mode=False,
+                 apply_chat_template=True, add_generation_prompt=False):
+        self.think_mode = think_mode
+        self.apply_chat_template = apply_chat_template
+        self.add_generation_prompt = add_generation_prompt
+        self.tokenizer = Tokenizer.from_file(str(file))
+        self.set_specials_ids()
+        self.set_eos_token()
+    def encode(self, text, chat_wrapped=None):
+        ids = []
+        chat_wrapped = chat_wrapped or self.apply_chat_template
+        if chat_wrapped:
+            text = self.wrap_chat(text)
+        stripped = text.strip()
+        if stripped in self.special_to_id and "\n" not in stripped:
+            return [self.special_to_id[stripped]]
+        for part in filter(None, self.SPLIT_RE.split(text)):
+            if part in self.special_to_id:
+                ids.append(self.special_to_id[part])
+            else:
+                ids.extend(self.tokenizer.encode(part).ids)
+        return ids
+    def decode(self, ids):
+        return self.tokenizer.decode(ids, skip_special_tokens=False)
+    def wrap_chat(self, user_msg):
+        s = f"<|im_start|>user\n{user_msg}<|im_end|>\n"
+        if self.add_generation_prompt:
+            s += "<|im_start|>assistant"
+            if self.think_mode:
+                s += "\n"
+            else:
+                s += "\n<think>\n\n</think>\n\n"
+        return s
+    def set_specials_ids(self):
+        self.special_to_id = {t: self.tokenizer.token_to_id(t) for t in self._SPECIALS}
+    def set_eos_token(self):
+        eos_token = "<|im_end|>" if self.think_mode else "<|endoftext|>"
+        self.eos_token_id = self.special_to_id[eos_token]
