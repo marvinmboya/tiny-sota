e049957fd49c215f5b3276b2ed8058d8e726f8e7
Marvin-desmond <marvindesmond@gmail.com>
2025-08-07 11:20:15 -0400

    implement modules like FeedForward, RMSNorm needed by Qwen

---
+++ src/tiny_sota/models/tiny_modules.py
@@ -0,0 +1,34 @@
+import torch, torch.nn as nn 
+import torch.nn.functional as F 
+
+from .configs import BaseConfig 
+
+class TriFeedForward(nn.Module):
+    def __init__(self, config: BaseConfig):
+        super(TriFeedForward,self).__init__()
+        emb_dim = config.emb_dim
+        hidden_dim = config.hidden_dim
+        bias = config.bias
+        dtype = config.dtype
+        self.w1 = nn.Linear(emb_dim, hidden_dim, bias=bias, dtype=dtype)
+        self.v = nn.Linear(emb_dim, hidden_dim, bias=bias, dtype=dtype)
+        self.w2 = nn.Linear(hidden_dim, emb_dim, bias=bias, dtype=dtype)
+    def forward(self,x):
+        x = F.silu(self.w1(x)) * self.v(x)
+        x = self.w2(x)
+        return x 
+    
+class RMSNorm(nn.Module):
+    def __init__(self, emb_dim, dtype=torch.float32, bias=False, eps=1e-6):
+        super(RMSNorm,self).__init__()
+        self.eps = eps 
+        self.weight = nn.Parameter(torch.ones(emb_dim, dtype=dtype))
+        self.bias = (nn.Parameter(torch.zeros(emb_dim, dtype=dtype)) 
+                    if bias else None)
+    def forward(self,x):
+        variance = x.pow(2).mean(dim=-1, keepdim=True)
+        norm_x = x * torch.rsqrt(self.eps + variance)
+        rms_norm = norm_x * self.weight
+        if self.bias is not None:
+            rms_norm += rms_norm + self.bias
+        return rms_norm
