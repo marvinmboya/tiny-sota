b6c327a94ec0c4c8903afec1ba25cf4ee6f7276e
Marvin-desmond <marvindesmond@gmail.com>
2025-08-09 22:00:53 -0400

    implement Llama3.2 architecture

---
+++ src/tiny_sota/models/llama_arch.py
@@ -0,0 +1,57 @@
+import torch
+
+from .tiny_modules import RMSNorm, TriFeedForward
+from .attention import GQAttention
+from .utils import compute_rope_params
+from .configs import BaseConfig
+
+
+config = Configs.Llama
+
+class DecoderBlock(nn.Module):
+    def __init__(self, config: BaseConfig, is_qwen3=True):
+        super(DecoderBlock,self).__init__()
+        emb_dim = config.emb_dim 
+        dtype = config.dtype
+        self.rms1 = RMSNorm(emb_dim, eps=1e-5)
+        self.rms2 = RMSNorm(emb_dim, eps=1e-5)
+        self.attn = GQAttention(config)
+        self.feed_forward = TriFeedForward(config)
+    def forward(self, x, mask, cos, sin):
+        shortcut = x
+        x = self.rms1(x)
+        x = self.attn(x, mask, cos, sin) + shortcut
+        shortcut = x
+        x = self.rms2(x)
+        x = self.feed_forward(x) + shortcut
+        return x
+    
+class Llama32Model(nn.Module):
+    def __init__(self, config: BaseConfig):
+        super(Llama32Model, self).__init__()
+        vocab = config.n_vocab
+        context_len = config.context_len
+        emb_dim = config.emb_dim
+        head_dim = config.head_dim
+        bias = config.bias
+        dtype = config.dtype
+        layers = config.layers
+        self.embedding = nn.Embedding(vocab, emb_dim, dtype=dtype)
+        self.decoders = nn.ModuleList([
+            DecoderBlock(config) for _ in range(layers)])
+        self.rms_norm = RMSNorm(emb_dim, eps=1e-5)
+        self.linear = nn.Linear(emb_dim, vocab, bias=bias, dtype=dtype)
+        cos, sin = compute_rope_params(head_dim, context_len, config.rope_base)
+        self.register_buffer("cos", cos, persistent=False)
+        self.register_buffer("sin", sin, persistent=False)
+
+    def forward(self,x):
+        x = self.embedding(x)
+        seq_len = x.shape[1]
+        mask = torch.triu(torch.ones(seq_len,seq_len, device=x.device, dtype=torch.bool), diagonal=1)
+        for decoder in self.decoders:
+            x = decoder(x, self.mask, self.cos, self.sin)
+        x = self.rms_norm(x)
+        out = self.linear(x)
+        return out
+
