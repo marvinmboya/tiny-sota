a243e5469ac186015d78a8f99894a8e2e75d8f42
Marvin-desmond <marvindesmond@gmail.com>
2025-08-10 00:48:09 -0400

    rename llama->llama3, Qwen->Qwen3

--- src/tiny_sota/models/__init__.py
+++ src/tiny_sota/models/__init__.py
@@ -4,13 +4,20 @@ from .attention import (
 
 from .configs import Configs
 from .qwen_arch import Qwen3Model
+from .llama_arch import Llama3Model 
+
 from .utils import getModelMemorySize
+
 from .qwen_load import (
-    fetchQwenWeightsAndTok, loadQwenWeightsAndTok, 
-    transferQwenWeights
+    fetchQwen3WeightsAndTok, 
+    loadQwen3WeightsAndTok,
+    transferQwen3Weights,
 )
+
 from .llama_load import (
-    fetchLlamaWeightsAndTok, loadLlamaWeightsAndTok
+    fetchLlama3WeightsAndTok, 
+    loadLlama3WeightsAndTok, 
+    transferLlama3Weights
 )
 
 from .tiny_load import showLocalWeights
