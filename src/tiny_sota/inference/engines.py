f6023e265459935ffd860c637b1e899091e8147e
Marvin-desmond <marvindesmond@gmail.com>
2025-08-11 12:25:16 -0400

    remove generate config, clean generate call

--- src/tiny_sota/inference/engines.py
+++ src/tiny_sota/inference/engines.py
@@ -4,7 +4,8 @@ from .tokenizers import (
     Llama3Tokenizer, Qwen3Tokenizer
 )
 from ..tiny_utils.display import bcolors
-from .utils import generate_text_stream, GenerateConfig 
+from ..tiny_utils import get_device
+from .utils import generate_text_stream
 
 @dataclass
 class TokenizerChoices:
@@ -20,20 +21,23 @@ def colorFlush(token, color=bcolors.NICE):
 
 class LLMEngine():
     def __init__(self, 
-                 loaded_model, 
-                 tokenizer_file: str, tokenizer_choice:TokenizerChoices, 
-                 **kwargs):
+        loaded_model, tokenizer_file: str, 
+        tokenizer_choice:TokenizerChoices, 
+        **kwargs):
         self.model = loaded_model.eval() 
-        self.tokenizer = loadTokenizer(tokenizer_file, tokenizer_choice, **kwargs) 
+        self.tokenizer = loadTokenizer(
+                            tokenizer_file, 
+                            tokenizer_choice, 
+                            **kwargs) 
         self.eos_token_id = self.tokenizer.eos_token_id
-    def __call__(self, prompt, g_config: GenerateConfig, is_hf_model=False):
-        max_new_tokens = g_config.max_new_tokens
-        context_len = g_config.context_len
-        device = g_config.device
+        self.max_new_tokens = 2000
+    def __call__(self, prompt):
+        max_new_tokens = self.max_new_tokens
+        device = get_device()
         tokens = self.tokenizer.encode(prompt)
         token_ids = torch.tensor(tokens,device=device).unsqueeze(0)
         for token in generate_text_stream(
             self.model, token_ids, max_new_tokens, 
-            context_len, eos_token_id=self.eos_token_id, is_hf_model=is_hf_model):
+            eos_token_id=self.eos_token_id):
             token_id = token.squeeze(0).tolist()
             colorFlush(self.tokenizer.decode(token_id))
