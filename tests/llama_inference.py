dd008fb88865398b3506a6cc38ff09b3645d2075
Marvin-desmond <marvindesmond@gmail.com>
2025-08-11 12:27:27 -0400

    remove generate config

--- tests/llama_inference.py
+++ tests/llama_inference.py
@@ -11,8 +11,7 @@ from tiny_sota.models import (
 )
 
 from tiny_sota.models.tiny_load import getLocalWeightsDir
-from tiny_sota.inference import GenerateConfig, TokenizerChoices, LLMEngine
-
+from tiny_sota.inference import TokenizerChoices, LLMEngine
 from tiny_sota.tiny_utils import get_device
 
 device = get_device()
@@ -27,14 +26,9 @@ transferLlama3Weights(model, config, weights)
 del weights
 model.to(device)
 
-generate_config = GenerateConfig(
-     context_len=config.context_len,
-     max_new_tokens=2000,
-     device = device)
-
 prompt = "the story of the princess and the lost castle"
-engine = LLMEngine(model, tok,
-                   TokenizerChoices.qwen,
-                   add_generation_prompt = True,
-                   think_mode = False)
-engine(prompt, generate_config, is_hf_model=False)
+engine = LLMEngine(
+          model, tok, TokenizerChoices.qwen,
+          add_generation_prompt = True,
+          think_mode = False)
+engine(prompt)
