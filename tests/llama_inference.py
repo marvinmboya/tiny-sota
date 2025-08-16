35ae8d865907511265e7a1506f3183f5b25db1b3
Marvin-desmond <marvindesmond@gmail.com>
2025-08-16 00:48:15 -0400

    fix inference pipeline run

--- tests/llama_inference.py
+++ tests/llama_inference.py
@@ -4,14 +4,13 @@ import torch.nn as nn
 
 
 from tiny_sota.models import (
-    fetchLlama3WeightsAndTok,
     loadLlama3WeightsAndTok,
     transferLlama3Weights,
     Llama3Model, ModelConfigs
 )
 
 from tiny_sota.models.tiny_load import getLocalWeightsDir
-from tiny_sota.inference import TokenizerChoices, LLMEngine
+from tiny_sota.inference import LLMEngine
 from tiny_sota.tiny_utils import get_device
 
 device = get_device()
@@ -20,15 +19,10 @@ config = ModelConfigs.Llama
 
 model = Llama3Model(config)
 
-fetchLlama3WeightsAndTok()
 weights, tok = loadLlama3WeightsAndTok()
 transferLlama3Weights(model, config, weights)
 del weights
-model.to(device)
 
 prompt = "the story of the princess and the lost castle"
-engine = LLMEngine(
-          model, tok, TokenizerChoices.qwen,
-          add_generation_prompt = True,
-          think_mode = False)
+engine = LLMEngine(model, tok, device)
 engine(prompt)
