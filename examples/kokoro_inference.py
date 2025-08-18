d791893bfa36bb905f4d80dc5312b42f6165f351
Marvin-desmond <marvindesmond@gmail.com>
2025-08-18 00:55:32 -0400

    clean imports

--- examples/kokoro_inference.py
+++ examples/kokoro_inference.py
@@ -1,17 +1,20 @@
 import torch 
 import torch.nn as nn 
-from transformers import AlbertConfig, AlbertModel as AlbertModel1
-from tiny_sota.models.configs import Albert
+from transformers import (
+    AlbertConfig as AlbertConfig1, 
+    AlbertModel as AlbertModel1
+)
+from tiny_sota.models.configs import AlbertConfig as AlbertConfig2
 from tiny_sota.models.kokoro_load import transferKokoroWeights
-from tiny_sota.models.kokoro_arch import AlbertModel  as AlbertModel2
+from tiny_sota.models.kokoro_arch import Albert  as AlbertModel2
 
 config = {"hidden_size": 768,"num_attention_heads": 12,
     "intermediate_size": 2048,"max_position_embeddings": 512,
     "num_hidden_layers": 12,"dropout": 0.1,"vocab_size": 178
 }
 
-alconf = AlbertConfig(**config)
-model = AlbertModel1(alconf)
+config1 = AlbertConfig1(**config)
+model = AlbertModel1(config1)
 
 def func(model, input, output):
     pass
@@ -20,8 +23,8 @@ model.pooler_activation.register_forward_hook(func)
 x = torch.randint(178, (1, 178), dtype=torch.long)
 out1 = model(x).last_hidden_state
 
-config = Albert()
-customalbert = AlbertModel2(config)
+config2 = AlbertConfig2()
+customalbert = AlbertModel2(config2)
 transferKokoroWeights(customalbert, config, model)
 out2, _ = customalbert(x)
 print(out1.shape)
