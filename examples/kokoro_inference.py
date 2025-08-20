9a7aa94da2bdfd3d94cbf80647fd2c87ae602996
Marvin-desmond <marvindesmond@gmail.com>
2025-08-20 12:31:34 -0400

    refine the inference for kokoro a bit more

--- examples/kokoro_inference.py
+++ examples/kokoro_inference.py
@@ -1,106 +1,29 @@
 import torch 
 import torch.nn as nn 
 
-from tiny_sota.models.kokoro_load import transferKokoroWeights, loadKokoroWeightsAndTok, check_generator_weights
+from tiny_sota.models.kokoro_load import loadKokoroWeightsAndVoice
+from tiny_sota.transfers import transferKokoroWeights
 from tiny_sota.models.kokoro_arch import KokoroConfig, Kokoro
 from tiny_sota.tiny_utils.core import get_device
-from misaki import en 
-from tiny_sota.models.tiny_load import fetchFilesHuggingFace
-from pathlib import Path
+from tiny_sota.inference.utils import generate_audio
+from tiny_sota.meta import KOKORO_VOICES
 
-device = get_device()
+import soundfile as sf 
+
+device = "cpu"
 config = KokoroConfig()
-model = Kokoro(config)
+model = Kokoro(config, device=device)
 
-import re
-lang_code='a' 
-g2p = en.G2P(trf=False, british=lang_code=='b', fallback=None, unk='')
-
-def load_voice(voice: str):
-    parent = Path.home()/".cache/tiny_sota"
-    local_dir = parent/"models"
-    fetchFilesHuggingFace(
-        repo_id="hexgrad/Kokoro-82M",
-        commit="8542409da2986c0ab5d41b3cf0411f7a58caab38",
-        rem_id=f"voices/{voice}.pt",
-        loc_id=f"{voice}.pt",
-        local_dir=local_dir
-    )
-    voice_weight = local_dir/f"{voice}.pt"
-    pack = torch.load(voice_weight, weights_only=True)
-    return pack
-
-def tokens_to_text(tokens):
-    return ''.join(t.text + t.whitespace for t in tokens).strip()
-
-def tokens_to_ps(tokens):
-    return ''.join(t.phonemes + (' ' if t.whitespace else '') for t in tokens).strip()
-
-def waterfall_last(tokens, next_count,
-        waterfall = ['!.?…', ':;', ',—'], 
-        bumps = [')', '”']):
-    for w in waterfall:
-        z = next((i for i, t in reversed(list(enumerate(tokens))) if t.phonemes in set(w)), None)
-        if z is None:
-            continue
-        z += 1
-        if z < len(tokens) and tokens[z].phonemes in bumps:
-            z += 1
-        if next_count - len(tokens_to_ps(tokens[:z])) <= 510:
-            return z
-    return len(tokens)
-
-def en_tokenize(tokens):
-    tks = []
-    pcount = 0
-    for t in tokens:
-        t.phonemes = '' if t.phonemes is None else t.phonemes
-        next_ps = t.phonemes + (' ' if t.whitespace else '')
-        next_pcount = pcount + len(next_ps.rstrip())
-        if next_pcount > 510:
-            z = waterfall_last(tks, next_pcount)
-            text = tokens_to_text(tks[:z])
-            ps = tokens_to_ps(tks[:z])
-            yield text, ps, tks[:z]
-            tks = tks[z:]
-            pcount = len(tokens_to_ps(tks))
-            if not tks:
-                next_ps = next_ps.lstrip()
-        tks.append(t)
-        pcount += len(next_ps)
-    if tks:
-        text = tokens_to_text(tks)
-        ps = tokens_to_ps(tks)
-        yield ''.join(text).strip(), ''.join(ps).strip(), tks
-
-def load_text(text: str, lang_code:str, pack: torch.Tensor, speed=1, model=None):
-    pattern = r'\n+'
-    text = re.split(pattern, text.strip())
-    for graphemes_index, graphemes in enumerate(text):
-        if not graphemes.strip():
-            continue
-        if lang_code in 'ab':
-            _, tokens = g2p(graphemes)
-            for gs, ps, tks in en_tokenize(tokens):
-                    if not ps:
-                        continue
-                    ps = ps[:510]
-                    audio, duration = model(ps, pack[len(ps)-1], speed)
-                    yield audio
-
-text = "The sky above the port was the color of television, tuned to a dead channel."
-voice='af_heart'
-pack = load_voice(voice).to(device)
-
-
-weights = loadKokoroWeightsAndTok()
+weights, voice = loadKokoroWeightsAndVoice(KOKORO_VOICES.MALE.FENRIR)
 transferKokoroWeights(model, config, weights)
 model.to(device)
 model.eval()
+pack = voice.to(device)
+del weights 
 
-import soundfile as sf 
-text = "In the end, everything sort of becomes a web of strings and a pot of chaos."
-for i, audio in enumerate(load_text(text, lang_code, pack, model=model)):
+lang_code='a'
+text = "Hi, hello. So we know that this channel is about self-expression, correct? We know that this channel is not giving therapy, it's not giving 100% correct advice, it is things that are rooted in my own life experience and things that I have learned from life, and it's advice that you can either take or leave and you need to listen to everything I say with a grain of salt. We're aware of this, that the internet is not a place you go for absolute truths, it's a place you go for perspectives, it's a place you go for opening your mind, looking at things a little bit differently. We're aware of that, right? I want to talk about the truth of being yourself."
+for i, audio in enumerate(generate_audio(text, lang_code, pack, model=model)):
     sf.write(f'{i}.wav', audio, 24000)
 
 
