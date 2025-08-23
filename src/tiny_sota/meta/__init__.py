e63c59b33eda17a559af4388e3422c41feb01fb0
Marvin-desmond <marvindesmond@gmail.com>
2025-08-23 03:14:42 -0400

    merge language codes and voices to make it easier

--- src/tiny_sota/meta/__init__.py
+++ src/tiny_sota/meta/__init__.py
@@ -10,46 +10,36 @@ def load_json(path):
 
 dataclass(frozen=True)
 class _FEMALE:
-    HEART = "af_heart"
-    ALLOY = "af_alloy"
-    AOEDE = "af_aoede"
-    BELLA = "af_bella"
-    JESSICA = "af_jessica"
-    KORE = "af_kore"
-    NICOLE = "af_nicole"
-    NOVA = "af_nova"
-    RIVER = "af_river"
-    SARAH = "af_sarah"
-    SKY = "af_sky"
+    AMERICAN_ENGLISH = "af_heart"
+    AMERICAN_ENGLISH2 = "af_bella"
+    BRITISH_ENGLISH = "bf_emma"
+    JAPANESE = "jf_alpha"
+    HINDI = "hf_alpha"
+    MANDARIN =  "zf_xiaobei"
+    SPANISH = "ef_dora"
+    BRAZ_PORT = "pf_dora"
+    FRENCH = "ff_siwis"
+    ITALIAN = "if_sara"
+
 
 @dataclass(frozen=True)
 class _MALE:
-    ADAM = "am_adam"
-    ECHO = "am_echo"
-    ERIC = "am_eric"
-    FENRIR = "am_fenrir"
-    LIAM = "am_liam"
-    MICHAEL = "am_michael"
-    ONYX = "am_onyx"
-    PUCK = "am_puck"
-    SANTA = "am_santa"
+    AMERICAN_ENGLISH = "am_fenrir"
+    AMERICAN_ENGLISH2 = "am_michael"
+    AMERICAN_ENGLISH3 = "am_puck"
+    BRITISH_ENGLISH = "bm_fable"
+    JAPANESE = "jm_kumo"
+    HINDI = "hm_omega"
+    MANDARIN =  "zm_yunjian"
+    SPANISH = "em_alex"
+    BRAZ_PORT = "pm_alex"
+    ITALIAN = "im_nicola"
+    
 
 @dataclass(frozen=True)
 class KOKORO_VOICES:
     FEMALE: _FEMALE = _FEMALE()
     MALE: _MALE = _MALE()
-
-@dataclass(frozen=True)
-class KOKORO_LANG_CODES:
-    AMERICAN_ENGLISH = 'a'
-    BRITISH_ENGLISH = 'b'
-    SPANISH = 'e'
-    FRENCH = 'f'
-    HINDI = 'h'
-    ITALIAN = 'i'
-    BRAZILIAN_PORTUGUESE = 'p'
-    JAPANESE = 'j'
-    MANDARIN = 'z'
 
 _root: Path = Path(__file__).parents[0]
 COCO_CLASSES = load_json(_root / "coco.txt")
