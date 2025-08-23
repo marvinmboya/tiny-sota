8ec8ec29d0dd72dd85c82e5b9f42fd7e3863ae7f
Marvin-desmond <marvindesmond@gmail.com>
2025-08-23 02:00:19 -0400

    integrate fully implemented tts engine inference

--- examples/kokoro_inference.py
+++ examples/kokoro_inference.py
@@ -1,29 +1,24 @@
-import torch 
-import torch.nn as nn 
-
+from tiny_sota.models.kokoro_arch import Kokoro, KokoroConfig
 from tiny_sota.models.kokoro_load import loadKokoroWeightsAndVoice
 from tiny_sota.transfers import transferKokoroWeights
-from tiny_sota.models.kokoro_arch import KokoroConfig, Kokoro
-from tiny_sota.tiny_utils.core import get_device
-from tiny_sota.inference.utils import generate_audio
-from tiny_sota.meta import KOKORO_VOICES
-
-import soundfile as sf 
+from tiny_sota.meta import KOKORO_VOICES, KOKORO_LANG_CODES
+from tiny_sota.inference import TTSEngine
 
 device = "cpu"
 config = KokoroConfig()
 model = Kokoro(config, device=device)
 
-weights, voice = loadKokoroWeightsAndVoice(KOKORO_VOICES.MALE.FENRIR)
+weights, voice = loadKokoroWeightsAndVoice(KOKORO_VOICES.FEMALE.HEART)
 transferKokoroWeights(model, config, weights)
-model.to(device)
-model.eval()
-pack = voice.to(device)
+model.to(device).eval()
 del weights 
 
-lang_code='a'
-text = "Hi, hello. So we know that this channel is about self-expression, correct? We know that this channel is not giving therapy, it's not giving 100% correct advice, it is things that are rooted in my own life experience and things that I have learned from life, and it's advice that you can either take or leave and you need to listen to everything I say with a grain of salt. We're aware of this, that the internet is not a place you go for absolute truths, it's a place you go for perspectives, it's a place you go for opening your mind, looking at things a little bit differently. We're aware of that, right? I want to talk about the truth of being yourself."
-for i, audio in enumerate(generate_audio(text, lang_code, pack, model=model)):
-    sf.write(f'{i}.wav', audio, 24000)
+engine = TTSEngine(
+    model, 
+    KOKORO_LANG_CODES.AMERICAN_ENGLISH,
+    device=device, 
+    voice=voice
+)
 
-
+text = "No man ever steps in the same river twice, for it's not the same river and he's not the same man."
+engine(text)
